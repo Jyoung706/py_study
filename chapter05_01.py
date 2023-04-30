c24fa39c566039e8da120f74cea7f78519e7abe9
jYoung706 <wnsdud9476@gmail.com>
2023-04-30 23:26:42 +0900

    [Add] chapter05-01 clear

--- chapter05_01.py
+++ chapter05_01.py
@@ -69,3 +69,75 @@ def func_mul2(x):
 p = func_mul2(50)
 
 print(type(p), p, p.get('v2'), p.items(), p.keys())
+print()
+
+# 중요
+# *args, **kwargs
+
+# *args(언팩킹)
+def args_func(*args): # 매개변수명은 자유
+    for i, v in enumerate(args):
+        print('Result : {}'.format(i), v)
+    print('-------')
+
+args_func('Lee')
+args_func('Lee','Park')
+args_func('Lee','Park','Kim')
+
+# **kwargs(언팩킹)
+def kwargs_func(**kwargs):
+    for v in kwargs.keys():
+        print("{}".format(v), kwargs[v])
+    print('-------')
+
+kwargs_func(name1 = 'Lee', name2 = 'Park', name3 = 'Cho')
+
+# 전체 혼합
+def example(args_1, args_2, *args, **kargs):
+    print(args_1, args_2, args, kargs)
+
+example(10, 20, 'Lee', 'Kim', 'Park', age1 = 20, age2 = 30, age3 = 40)
+
+# 중첩 함수
+
+def nested_func(num):
+    def func_in_func(num):
+        print(num)
+    print('In func')
+    func_in_func(num + 100)
+
+nested_func(100)
+
+# func_in_func(100) 은 실행 불가
+# nested_func 안에서 정의된 함수이기 때문에
+
+# 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 => 리소스(메모리) 할당
+# 람다는 즉시 실행 함수(Heap 초기화) => 메모리 초기화
+# 남발 시 가독성 오히려 감소
+
+# def mul_func(x,y):
+#    return x * y
+
+# a = lambda x,y: x*y
+
+# 일반적인 함수 => 할당
+def mul_func(x,y):
+    return x * y
+
+q = mul_func(10,50)
+print(q)
+
+mul_func_var = mul_func
+print(mul_func_var(20,50))
+
+# 람다 함수 => 할당
+lamda_mul_func = lambda x,y:x*y
+
+print(lamda_mul_func(50,50))
+
+def func_final(x,y, func):
+    print('>>>>>>>>',x*y*func(100,100))
+
+func_final(10,20,lambda x,y:x*y)
